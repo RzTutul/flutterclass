14b5234fb32d6a3a011397149638b42d472b33ee
RzTutul <rz.tutul@gmail.com>
2023-05-14 22:05:40 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ContractPage(),
+    );
+  }
+}
+
+
+class ContractPage extends StatelessWidget {
+   ContractPage({Key? key}) : super(key: key);
+
+ List<String> phone =["01924250146","018454654654","89654654654","56465465465","145465654654","123412412432"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: const Text("Contract List"),
+      ),
+        body:  SingleChildScrollView(
+          child: Column(
+            children: [
+              //for text
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("This is my contract",style: TextStyle(fontSize: 15,color: Colors.blueGrey),),
+                ),
+              ),
+              //image,phone number
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                  itemCount: phone.length,
+                  itemBuilder: (context,position){
+              return  Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          child: Image.network("https://media.licdn.com/dms/image/C5603AQGNJvU8H1TwhQ/profile-displayphoto-shrink_800_800/0/1614058429065?e=2147483647&v=beta&t=RDt68jE35OfezhX4Bpeb40ZSsspfF_RJPJznpg2L0s8"
+                            ,width: 50,height: 50,
+                          ),
+                        ),
+                      ),
+                       Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Text(phone[position],style: TextStyle(fontSize: 16,color: Colors.black),),
+                      )
+                    ],
+                  ),
+                );
+              })
+
+            ],
+          ),
+        )
+    );
+  }
+}
+
+
